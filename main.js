f4f976c9b9f25713347a0bbf729bc7a2bd1f4b4d
WebBlade <monctrikua@gmail.com>
2025-10-18 02:36:01 +0300

    final version

--- main.js
+++ main.js
@@ -1,39 +1,77 @@
-import {Command} from "commander"
+import { Command } from "commander"
 import http from "http"
-import fs from "fs"
+import fs from "fs/promises"
+import { XMLBuilder } from "fast-xml-parser"
+import { URL } from "url"
 
 const program = new Command()
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from Node.js server!");
-});
-
 program
-    .name("WebBack-4")
-    .description("")
-    .version("1.0.0")
+  .name("WebBack-4")
+  .description("")
+  .version("1.0.0")
+  .requiredOption("-p, --port <number>", "Server port")
+  .option("-i, --input [file]", "Input JSON file path (iris.json)")
+  .requiredOption("-h, --host <string>", "Server host", "localhost")
+  .action(async (options) => {
+    if (options.input === true || !options.input) {
+      console.error("Please, specify input file");
+      process.exit(1);
+    }
 
-program
-    .option("-i --input <file>", "Input file path")
-    .requiredOption("-h --host <string>", "Server host")
-    .requiredOption("-p --port <number>", "Server port")
-    .action(options => {
-        if (options.input === true || !options.input) {
-        console.error("Please, specify input file");
-        process.exit(1);
-        }
+    try {
+      await fs.access(options.input);
+    } catch (error) {
+      console.error("Cannot find input file");
+      process.exit(1);
+    }
 
-        if(!fs.existsSync(options.input)){
-        console.error("Cannot find input file")
-        process.exit(1)
-        }
-
-        server.listen(options.port,() => {
-        console.log(`Server is running on port ${options.port}`)
-        })
+    const builder = new XMLBuilder({
+      format: true
     })
 
+    const server = http.createServer(async (req, res) => {
+        const requestUrl = new URL(req.url, `http://${options.host}:${options.port}`)
+        try {
+            const params = requestUrl.searchParams
+            const fileContent = await fs.readFile(options.input, "utf-8")
+            const lines = fileContent.split(/\r?\n/).map(l => l.trim()).filter(Boolean)
+            let jsonData = lines.map(line => JSON.parse(line))
 
-program.parse(process.args)
-const options = program.opts()
+            const showVariety = params.get('variety') === 'true'
+            const minPetalLengthStr = params.get('min_petal_length')
+            const minPetalLengthNum = parseFloat(minPetalLengthStr)
+
+            if (!isNaN(minPetalLengthNum)) {
+                jsonData = jsonData.filter(flower => {
+                    return flower['petal.length'] > minPetalLengthNum
+                })
+            }
+
+            const mappedData = jsonData.map(flower => {
+                const obj = {
+                    petal_length: flower['petal.length'],
+                    petal_width: flower['petal.width']
+                }
+            if (showVariety) obj.variety = flower.variety
+            
+            return obj
+            })
+
+        const xmlContent = builder.build({flowers:{ flower: mappedData }})
+        res.writeHead(200, { "Content-Type": "application/xml; charset=utf-8" })
+        res.end(xmlContent)
+
+      } catch (error) {
+        res.writeHead(500, { "Content-Type": "text/plain" })
+        res.end("Internal Server Error")
+      }  
+    })
+
+    server.listen(options.port, options.host, () => {
+      console.log(`Server is started: http://${options.host}:${options.port}`)
+    })
+
+  })
+
+program.parse(process.argv)
